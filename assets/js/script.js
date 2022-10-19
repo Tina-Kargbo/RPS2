155f76f41cc135b3a46f156b167c64c809a1b585
Tina-Kargbo <cl.amankwaah@gmail.com>
2022-10-19 10:25:42 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+/**
+ * Declare constants for DOM elements
+ * and possible choices.
+ */
+ const buttons = document.getElementsByClassName("control");
+ const buttons = document.getElementsById("player-score");
+ const buttons = document.getElementsById("computer-score");
+ const buttons = document.getElementsById("player-image");
+ const buttons = document.getElementsById("computer-image");
+ const buttons = document.getElementsById["rock", "paper", "scissors"]; 
+ const buttons = document.getElementsById("rock");
+ const buttons = document.getElementsById("paper");
+ const buttons = document.getElementsById("scissors");
+ const buttons = document.querySelectorAll("button");
+
+ document.getElementsById
+ /** 
+  * Add event listener to all the buttons
+  */
+ 
+ for (let button of buttons) {
+     button.addEventListener("click", function () {
+         let playerChoice = this.getAttribute("data-choice");
+         playGame(playerChoice);
+     });
+ }
+ 
+ /**
+  * The main game function. Accepts one parameter, 
+  * which is the data-choice value of the selected button.
+  */
+ function playGame(playerChoice) {
+ 
+     playerImage.src = `assets/imagesimages/${choices[playerChoice]}.png`;
+     playerImage.alt = choices[playerChoice];
+ 
+     let computerChoice = Math.floor(Math.random() * 3);
+ 
+     computerImage.src = `assets/imagesimages/${choices[computerChoice]}.png`;
+     computerImage.alt = choices[computerChoice];
+ 
+     let result = checkWinner(choices [computerChoice], choices[playerChoice]);
+ 
+     updateScores(result);
+ }
+ 
+ /**
+  * Checks to see who the winner is, it accepts two strings as
+  */
